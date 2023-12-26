b5e2692647de74ff0b6184c53c8c3dbe03face3a
qurbonovozodbek <ozodbekkurbonov185@gmail.com>
2023-12-26 17:52:07 +0500

    masalalar

---
+++ app.js
@@ -0,0 +1,76 @@
+// let a  = [7, 2, 4, 6]
+// a.sort(function(a, b) {
+//     return b - a;
+// })
+// console.log(a[1]); 
+
+// let a = [2, 4, 5, 6, 7, 8, 9]
+// a.splice(4, 2, 'salom')
+// console.log(a);
+
+// let a = ['olma', 'alma', 'ozodbek', 'goood']
+// a.slice(2);
+// console.log(a);
+
+// let a = [2, 4, 5, 6, 7, 8, 9]
+// let b = a.filter((i) => {
+//     return i > 6;
+// })
+// console.log(b); 
+
+// let a = [2, 4, 5, 6, 7, 8, 9]
+// let b = a.map((i) => {
+//     return i ** 2;
+// })
+// console.log(b);
+
+// let a = [1, 8, 9, 64, 25]
+// let b = a.map((i) => {
+//     return 
+// })
+
+// let a = [1, 8, 9, 64, 25]
+
+// let b = a.some((v) => {
+//     return v > 1;
+// })
+// console.log(b);
+
+let a = [2, 8, 9, 64, 25]
+
+let b = a.every((v) => {
+    return v > 1; 
+})
+console.log(b);
+
+// let ser = {
+//     name: 'ozodbek',
+//     age: 16,
+//     email: 'ozodbek@gmail.com',
+//     call: ['+6498468468']
+// }
+// // ser.name = 'ogabek'
+// ser.age = 24;
+// ser.likeColor = "green"
+// console.log(ser.likeColor);
+
+// const car = {
+//     name: 'lanbarghini',
+//     year: '1974',
+//     contry: 'Italy',
+//     about: function() {
+//         console.log(this.name + ' ' + this.year + ' ' + this.contry);
+//     }
+// }
+// car.about();
+
+// function User(name, age, email, phone) {
+//     this.name = name;
+//     this.age = age;
+//     this.email = email;
+//     this.phone = phone;
+// }
+// const doe = new User('doe', 22, 'doi@gmail.com', '+987462486');
+// const sad = new User('dfgbe', 24, 'gbdfb@gmail.com', '+56765867');
+
+// console.log(doe, sad);
